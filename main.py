67665af18971933b76641300e4733e29654ea4f7
Dudistar <90912152+Dudistar@users.noreply.github.com>
2025-12-25 12:13:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-import telebot
 
-# הפרטים שלך
-BOT_TOKEN = "8575064945:AAH_2WmHMH25TMFvt4FM6OWwfqFcDAaqCPw"
-
-print("בודק חיבור לטלגרם...")
-try:
-    bot = telebot.TeleBot(BOT_TOKEN)
-    me = bot.get_me()
-    print(f"✅ הצלחה! מחובר בתור: {me.first_name}")
-    print("הטוקן תקין. הבעיה היא בקוד הגדול.")
-except Exception as e:
-    print(f"❌ שגיאה: הטוקן לא תקין או חסום.")
-    print(f"פירוט: {e}")
